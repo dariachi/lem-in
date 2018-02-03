776c2c7a2b1a0714fb527ceeea19f88183f28f6a
Ruslan <ruslan.gulam.n@gmail.com>
2018-02-03 16:25:26 +0200

    my.sh

---
+++ my.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+echo "1_m.txt"
+./cmake-build-debug/lem_in < maps/1_m.txt
+echo " "
+echo "2_m.txt"
+./cmake-build-debug/lem_in < maps/2_m.txt
+echo " "
+echo "3_m.txt"
+./cmake-build-debug/lem_in < maps/3_m.txt
+echo " "
+echo "4_m.txt"
+./cmake-build-debug/lem_in < maps/4_m.txt
+echo " "
+echo "5_m.txt"
+./cmake-build-debug/lem_in < maps/5_m.txt
+echo "6_m.txt"
+./cmake-build-debug/lem_in < maps/6_m.txt
+echo " "
+echo "7_m.txt"
+./cmake-build-debug/lem_in < maps/7_m.txt
+./cmake-build-debug/lem_in < maps/8_m.txt
+./cmake-build-debug/lem_in < maps/9_m.txt
+./cmake-build-debug/lem_in < maps/10_m.txt
+./cmake-build-debug/lem_in < maps/11_m.txt
+./cmake-build-debug/lem_in < maps/12_m.txt
+./cmake-build-debug/lem_in < maps/13_m.txt
+./cmake-build-debug/lem_in < maps/14_m.txt
+./cmake-build-debug/lem_in < maps/15_m.txt
+./cmake-build-debug/lem_in < maps/16_m.txt
+./cmake-build-debug/lem_in < maps/17_m.txt
+./cmake-build-debug/lem_in < maps/18_m.txt
+./cmake-build-debug/lem_in < maps/19_m.txt
+./cmake-build-debug/lem_in < maps/20_m.txt
+./cmake-build-debug/lem_in < maps/21_m.txt
+./cmake-build-debug/lem_in < maps/22_m.txt
+./cmake-build-debug/lem_in < maps/23_m.txt
+./cmake-build-debug/lem_in < maps/24_m.txt
+./cmake-build-debug/lem_in < maps/25_m.txt
+./cmake-build-debug/lem_in < maps/26_m.txt
+./cmake-build-debug/lem_in < maps/27_m.txt
+./cmake-build-debug/lem_in < maps/28_m.txt
+./cmake-build-debug/lem_in < maps/29_m.txt
+./cmake-build-debug/lem_in < maps/30_m.txt
+./cmake-build-debug/lem_in < maps/31_m.txt
+./cmake-build-debug/lem_in < maps/32_m.txt
+./cmake-build-debug/lem_in < maps/33_m.txt
+#./cmake-build-debug/lem_in < maps/34_m.txt
+
+./cmake-build-debug/lem_in < maps/Lem_in_Kiev_subway.txt
+./cmake-build-debug/lem_in < maps/algo.txt
+./cmake-build-debug/lem_in < maps/algo_2.txt
+./cmake-build-debug/lem_in < maps/algo_3.txt
+./cmake-build-debug/lem_in < maps/algo_4.txt
+./cmake-build-debug/lem_in < maps/algo_divided.txt
+./cmake-build-debug/lem_in < maps/algo_gen7.txt
+./cmake-build-debug/lem_in < maps/algo_hard_abc.txt
+./cmake-build-debug/lem_in < maps/example.txt
+./cmake-build-debug/lem_in < maps/example_2.txt
+./cmake-build-debug/lem_in < maps/example_3.txt
+
+./cmake-build-debug/lem_in < maps/example_4.txt
+./cmake-build-debug/lem_in < maps/gen_11.txt
+./cmake-build-debug/lem_in < maps/hpj.txt
+./cmake-build-debug/lem_in < maps/map10.txt
+./cmake-build-debug/lem_in < maps/pdf_1.txt
+./cmake-build-debug/lem_in < maps/pdf_2.txt
+./cmake-build-debug/lem_in < maps/t.txt
+./cmake-build-debug/lem_in < maps/test.txt
+./cmake-build-debug/lem_in < maps/test_1.txt
+./cmake-build-debug/lem_in < maps/test_illia.txt
+./cmake-build-debug/lem_in < maps/test_illia_2.txt
+./cmake-build-debug/lem_in < maps/to_test.txt
+./cmake-build-debug/lem_in < maps/tra.txt
+./cmake-build-debug/lem_in < maps/ttt.txt
+./cmake-build-debug/lem_in < maps/visual.txt
+
